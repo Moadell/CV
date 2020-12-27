a2f6f6dae7186ad0dca8af9e8ddb178323564dfa
Moadell <mo.adel@nu.edu.eg>
2020-12-27 22:48:26 +0200

    Updates

---
+++ precache-manifest.54598e019d079c8df593bdeec7e257d8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f25414024c5830e1279365ce5780478",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "274ba2bc296ce57fe33f",
+    "url": "/CV/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "aa9bc45bf618ec914243",
+    "url": "/CV/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "af0aedc71bbfb3431019",
+    "url": "/CV/static/css/main.ab6a9126.chunk.css"
+  },
+  {
+    "revision": "c20d63c3157680012112",
+    "url": "/CV/static/js/2.c8a8b13b.chunk.js"
+  },
+  {
+    "revision": "d0d8190170b812713050bd2e6e947e19",
+    "url": "/CV/static/js/2.c8a8b13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274ba2bc296ce57fe33f",
+    "url": "/CV/static/js/3.e45ae94c.chunk.js"
+  },
+  {
+    "revision": "aa9bc45bf618ec914243",
+    "url": "/CV/static/js/4.ff4061e8.chunk.js"
+  },
+  {
+    "revision": "af0aedc71bbfb3431019",
+    "url": "/CV/static/js/main.a15fb957.chunk.js"
+  },
+  {
+    "revision": "85ab8421aea81d4c90f4",
+    "url": "/CV/static/js/runtime-main.de5786f1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/CV/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ce6a76ae91ad382e95b67d91b296c3a7",
+    "url": "/CV/static/media/IBM.ce6a76ae.svg"
+  },
+  {
+    "revision": "8a68e7b17489c5df4f5df16a642bd301",
+    "url": "/CV/static/media/IH.8a68e7b1.jpg"
+  },
+  {
+    "revision": "67db194b531996eda39dbb1247c728d1",
+    "url": "/CV/static/media/MM.67db194b.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/CV/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "538fb5f98185c028f03ef518adb93dc7",
+    "url": "/CV/static/media/VOIS.538fb5f9.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/CV/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/CV/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c7ab52fae3e3fc8f5d5233e3c3481488",
+    "url": "/CV/static/media/ex.c7ab52fa.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/CV/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/CV/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/CV/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
